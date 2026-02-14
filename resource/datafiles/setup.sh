89f48c10fe40595a1e0bb14592efb8a6e4e2f8c0
Alkapivo <alkapivo@gmail.com>
2026-02-14 22:20:03 +0100

    Add VERBOSE to setup.sh

--- resource/datafiles/setup.sh
+++ resource/datafiles/setup.sh
@@ -1,7 +1,28 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
+echo -e "\n\n⬆️  Setup is starting\n===================="
+
+# arguments
+VERBOSE=""
+
+# parse arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  -v|--verbose)
+    VERBOSE="-v"
+    shift
+    ;;
+  *)
+    echo -e "\nUnknown option: $1\n"
+    exit 1
+    ;;
+  esac
+done
 
 # install track in datafiles
-echo "\n🔧 Install track folder\n========================"
+echo -e "\n🔧 Install track folder\n========================"
 rm -rf ./track
-cp -rv ./gm_modules/track/resource/datafiles/track/ ./track/
+cp -r $VERBOSE ./gm_modules/track/resource/datafiles/track/ ./track/
+ls -1 ./track/
 
+echo -e "\n\n✅ The setup is finished\n========================"
